b285914c47481167ca34eaaf71b48b02c1e3fe97
Dennis Gera <dennis.gera@bain.com>
2025-03-26 09:41:53 -0300

    feat: update files

--- config.ts
+++ config.ts
@@ -14,3 +14,5 @@ if (!privateKey) throw new Error(`Expected env var SUPABASE_API_KEY`);
 const url = process.env.SUPABASE_URL;
 if (!url) throw new Error(`Expected env var SUPABASE_URL`);
 export const supabase = createClient(url, privateKey);
+
+
